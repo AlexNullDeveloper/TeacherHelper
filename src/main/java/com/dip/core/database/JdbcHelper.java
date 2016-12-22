0b6a1f64506bb5bdee1180dd3d4f18d099c796ea
ru.talisman.tal <talismanoff1990@yandex.ru>
2016-12-22 16:54:26 +0300

    extracted main method for test

--- src/main/java/com/dip/core/database/JdbcHelper.java
+++ src/main/java/com/dip/core/database/JdbcHelper.java
@@ -11,28 +11,16 @@ public class JdbcHelper {
 
     public static Connection getConnection() {
 
-//        String dbName = "XE";
-//        String dbURL="jdbc:oracle:thin:@talismanov-pc:1521/" + dbName;
         Locale.setDefault(new Locale("en", "US"));
         Connection connection = null;
         try {
             Class.forName("oracle.jdbc.driver.OracleDriver");
             connection = DriverManager.getConnection(DB_URL, DB_USER_LOGIN, DB_USER_PASSWORD);
-        } catch (SQLException e) {
+        } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
 
         return connection;
-    }
-
-    public static void main(String[] args) throws SQLException {
-        Connection connection = JdbcHelper.getConnection();
-        PreparedStatement preparedStatement = connection.prepareStatement("SELECT USER FROM DUAL");
-        ResultSet resultSet = preparedStatement.executeQuery();
-        if (resultSet.next()) {
-            System.out.println("resultset result " + resultSet.getString(1));
-        }
     }
 }
